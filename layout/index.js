01309985db802e02fb6fb627b19947591192bbb4
huudongg25 <thdong98225@gmail.com>
2022-07-05 11:28:43 +0700

    fix

--- layout/index.js
+++ layout/index.js
@@ -1,6 +1,6 @@
 import classNames from "classnames/bind";
 import styles from "./Layout.module.scss";
-import Header from "../components/Header";
+import Header from "../components/header";
 import Navbar from "../components/navbar";
 import { children } from "react";
 
